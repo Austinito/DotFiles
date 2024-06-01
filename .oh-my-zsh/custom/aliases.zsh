4a290e3126774df9cd3e257aaf10fab7d594b19c
Austinito <austinito@github.com>
2024-06-01 11:48:04 -0700

    Clean up unused .tmux and oh-my-zsh

--- .oh-my-zsh/custom/aliases.zsh
+++
@@ -1,5 +0,0 @@
-# Custom Scripts/Aliases
-alias my_gbpurge='git branch --no-track | grep -Ev "(\*|master|develop)" | sed 's/+//' | xargs -n 1 git branch -d'
-alias my_config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-alias gw='git worktree'
-alias :q='exit'
--- .oh-my-zsh/custom/exports.zsh
+++
@@ -1,8 +0,0 @@
-export PATH="$HOME/.local/bin:$PATH"
-#export PATH="$HOME/Library/Python/2.7/bin:$PATH"
-export PATH="$HOME/Library/Python/3.10/bin:$PATH"
-export PATH="$HOME/workspace/scripts/bin:$PATH"
-
-export CHROMEDRIVER_PATH="$HOMEBREW_PREFIX/bin/chromedriver"
-
-export PUBLIC_HOST="localhost"
