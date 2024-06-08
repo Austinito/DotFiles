fe4d93545db5e3ce4f7981fd1eb4ae1d179525bc
Austinito <austinito@github.com>
2024-06-08 10:07:11 -0700

    Add codeium settings

---
+++ .config/nvim/lua/configs/codeium.lua
@@ -0,0 +1,8 @@
+-- Commands to configure and setup codeium
+-- https://github.com/Exafunction/codeium
+--vim.g.codeium_filetypes = {\
+--    \ 'bash': v:false,
+--    \ 'typescript': v:true,
+--    \ }
+vim.g.codeium_enabled = true
+vim.g.codeium_manual = true
